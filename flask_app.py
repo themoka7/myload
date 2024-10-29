9dc370c88fe558237ecaa9378d50670843fd3fa4
정목 <jm3280@pusan.ac.kr>
2024-10-29 23:25:54 +0900

    20241023

--- flask_app.py
+++ flask_app.py
@@ -30,7 +30,7 @@ def hexagram():
 @app.route('/hexagram/result')
 def hexagram_result():
     # 세션에 저장된 처리된 데이터 불러오기
-    session.clear()
+
     data = session.get('data', {})
     return render_template('hexagram/hexagram_result.html', data=data)  # 결과 페이지에서 데이터 표시
 
@@ -51,7 +51,7 @@ def chizodiac():
 @app.route('/chizodiac/result')
 def chizodiac_result():
     chizodiac_result = session.get('chizodiac_result', None)
-    session.clear()
+
     data = session.get('data', {})
     return render_template('chizodiac/chizodiac_result.html', chizodiac_result=chizodiac_result)  # 결과 페이지에서 데이터 표시
 
@@ -66,6 +66,7 @@ def chizodiac_process():
 
     data = get_chizodiac_data(chizodiac_data)
 
+    session.clear()
     # 받은 card_data를 처리 (여기서는 단순 출력)
     session['chizodiac_result'] = data
     return jsonify({'redirect': url_for('chizodiac_result')})
@@ -79,7 +80,7 @@ def tarot():
 @app.route('/tarot/result')
 def tarot_result():
     tarot_result = session.get('tarot_result', None)
-    session.clear()
+
     data = session.get('data', {})
     return render_template('tarot/tarot_result.html', tarot_result=tarot_result)  # 결과 페이지에서 데이터 표시
 
