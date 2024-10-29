faf0bce0c6c5e091094dd7c77767482dd586441a
themoka7 <jm3280@pusan.ac.kr>
2024-10-29 15:22:15 +0900

    go home

--- getgit.sh
+++ getgit.sh
@@ -1,3 +1,7 @@
 #git clone https://github.com/themoka7/myload.git myloadgit
-
+#rsync -avh --exclude='.git' myloadgit/ ./
+#그 이후 소스 가져오기
+cd /home/kimjungmok/mysite/myloadgit
+git pull origin main
+cd ..
 rsync -avh --exclude='.git' myloadgit/ ./
